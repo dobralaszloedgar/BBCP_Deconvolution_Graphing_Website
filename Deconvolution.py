13f19003c78929c2160ab53677fe6bcf7a44d6b8
Edgar Dobra <dobralaszloedgar@gmail.com>
2025-09-17 19:34:14 -0500

    Launcher v1.2.10.5

--- Deconvolution.py
+++ Deconvolution.py
@@ -118,7 +118,7 @@ def run_deconvolution(
             if not PYBASELINES_AVAILABLE:
                 raise ImportError("pybaselines is required for asls baseline correction")
             baseline_fitter = Baseline()
-            baseline = baseline_fitter.arpls(y, lam=1e9, tol=1e-5, max_iter=3)[0]
+            baseline = baseline_fitter.arpls(y, lam=1e13, tol=1e-5, max_iter=3)[0]
             return y - baseline, baseline
 
         ref_points = []
